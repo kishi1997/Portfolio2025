be696fe01c2f8acf335fa60ee77d426ce1b2fa46
kishi1997 <tomo9634@gmail.com>
2025-03-21 16:18:09 -0700

    NavbarコンポーネントのコンタクトボタンにOvoフォントを適用しました。

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -58,7 +58,7 @@ const Navbar = () => {
           </button>
           {/* コンタクトボタン */}
           <Link
-            className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4"
+            className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo"
             href="#contact"
           >
             CONTACT{" "}
