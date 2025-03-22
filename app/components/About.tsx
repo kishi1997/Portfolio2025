e1a0b3b66b1248369829d93ee56680625b973669
kishi1997 <tomo9634@gmail.com>
2025-03-21 23:41:11 -0700

    Navbarコンポーネントにモバイルメニューを追加し、スクロール時の背景色を変更。グローバルCSSに新しいカスタムプロパティを追加し、ページコンポーネントにAboutとHeaderを追加しました。

---
+++ app/components/About.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Image from "next/image";
+import { assets, infoList, toolsData } from "@/assets/assets";
+const About = () => {
+  return (
+    <div id="about" className="w-full px-[12%] py-10 scroll-mt-20">
+      <h4 className="text-center mb-2 text-lg font-Ovo">Introduction</h4>
+      <h2 className="text-center text-5xl font-Ovo">About me</h2>
+      <div className="flex w-full flex-col lg:flex-row items-center gap-20 my-20">
+        <div className="w-64 sm:w-80 rounded-3xl max-w-none">
+          <Image
+            src={assets.user_image}
+            width={400}
+            height={538}
+            alt="user"
+            className="w-full rounded-3xl"
+          />
+        </div>
+        <div className="flex-1">
+          <p className="mb-10 max-w-2xl font-Ovp">
+            I am a front-end web developer who is currently based in Vancouver,
+            Canada. I have a passion for creating visually stunning and
+            user-friendly websites.
+          </p>
+          <ul className="grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl">
+            {infoList.map((info, index) => (
+              <li
+                key={index}
+                className="border-[0.5px] border-gray-400 p-6 rounded-xl cursor-pointer hover:bg-lightHover hover:-translate-y-1 hover:shadow-black duration-500"
+              >
+                <Image
+                  src={info.icon}
+                  width={0}
+                  height={0}
+                  alt={info.title}
+                  className="w-6"
+                />
+                <h3 className="my-4 font-semibold text-gray-700">
+                  {info.title}
+                </h3>
+                <p className="text-gray-600 text-sm">{info.description}</p>
+              </li>
+            ))}
+          </ul>
+          <h4 className="my-6 text-gray-700 font-Ovo">Tools I use</h4>
+          <ul className="flex gap-3 sm:gap-5 items-center">
+            {toolsData.map((tool, index) => (
+              <li
+                key={index}
+                className="flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500"
+              >
+                <Image
+                  src={tool}
+                  width={0}
+                  height={0}
+                  alt="tool"
+                  className="w-5 sm:w-7"
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ app/components/Header.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import Image from "next/image";
+import { assets } from "@/assets/assets";
+import Link from "next/link";
+const Header = () => {
+  return (
+    <div className="h-screen w-11/12 max-w-3xl mx-auto text-center flex flex-col items-center justify-center gap-4">
+      <div>
+        <Image
+          className="rounded-full w-32"
+          src={assets.profile_img}
+          width={158}
+          height={158}
+          alt="profile-img"
+        />
+      </div>
+      {/* 自己紹介 */}
+      <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo">
+        Hi! I&apos;m Tomoyuki Kishi
+        <Image
+          className="rounded-full w-6"
+          src={assets.hand_icon}
+          width={0}
+          height={0}
+          alt="hand-icon"
+        />
+      </h3>
+      <h1 className="text-3xl sm:text-6xl lg:text:[66px] font-Ovo">
+        Frontend web developer based in Vancouver.
+      </h1>
+      <p className="max-w-2xl mx-auto front-Ovo">
+        I am a front-end web developer who is currently based in Vancouver,
+        Canada. I have a passion for creating visually stunning and
+        user-friendly websites.
+      </p>
+      <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
+        {/* CVボタン */}
+        <Link
+          href="#contact"
+          className="px-10 py-3 border border-white bg-black text-white rounded-full flex items-center gap-2"
+        >
+          Contact me
+          <Image
+            className="w-4"
+            src={assets.right_arrow_white}
+            width={0}
+            height={0}
+            alt="right-arrow-white"
+          />
+        </Link>
+        {/* resumeダウンロードボタン */}
+        <Link
+          href="/sample-resume.pdf"
+          download
+          className="px-10 py-3 border border-gray-500 rounded-full flex items-center gap-2"
+        >
+          My resume
+          <Image
+            className="w-4"
+            src={assets.download_icon}
+            width={0}
+            height={0}
+            alt="download-icon"
+          />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
