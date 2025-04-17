038d723f42949e3d30e12bcf055aac39a3d691f2
kishi1997 <tomo9634@gmail.com>
2025-04-16 17:29:43 -0700

    next.config.tsの出力設定を変更し、AboutコンポーネントからisDarkModeプロパティを削除しました。各コンポーネントで画像のパスをBASE_PATHを使用して修正し、未使用のコードを削除しました。

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import Image from "next/image";
 import { assets } from "@/assets/assets";
 import Link from "next/link";
+import nextConfig from "../../next.config";
+const BASE_PATH = nextConfig.basePath || "";
 
 type FooterProps = {
   isDarkMode: boolean;
@@ -13,7 +15,11 @@ const Footer = ({ isDarkMode }: FooterProps) => {
     <div className="mt-20">
       <div className="text-center">
         <Image
-          src={isDarkMode ? assets.logo_dark : assets.logo}
+          src={
+            isDarkMode
+              ? `${BASE_PATH}/${assets.logo_dark}`
+              : `${BASE_PATH}/${assets.logo}`
+          }
           className="w-36 mx-auto mb-2"
           alt="logo"
           width={0}
@@ -21,7 +27,7 @@ const Footer = ({ isDarkMode }: FooterProps) => {
         />
         <div className="w-max flex items-center gap-2 mx-auto">
           <Image
-            src={assets.mail_icon}
+            src={`${BASE_PATH}/${assets.mail_icon}`}
             className="w-6"
             alt="mail-icon"
             width={0}
